8e68895b8d19c403907519ce862296a1928e2f1e
selimalahi <alahi.selim@gmail.com>
2022-12-10 23:05:34 +0600

    portfolio update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Contact from "./component/Contact";
+import Experience from "./component/Experience";
+import Header from "./component/Header";
+import Home from "./component/Home";
+import Portfolio from "./component/Portfolio";
+import SocialLinks from "./component/SocialLinks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Header></Header>
+    <Home></Home>
+    <SocialLinks></SocialLinks>
+    <Portfolio></Portfolio>
+    <Experience></Experience>
+    <Contact></Contact>
+   </div>
   );
 }
 
